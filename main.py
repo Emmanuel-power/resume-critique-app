25e68eb0c44f10c8a28ba7fbb8d5196481b4b246
Emmanuel Ogbeide <eogbeide08@gmail.com>
2026-04-05 21:52:30 -0400

    Change layout of page configuration to centered

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-st.set_page_config(page_title="AI Resume Critiquer", page_icon="", layout="left")
+st.set_page_config(page_title="AI Resume Critiquer", page_icon="", layout="centered")
 
 st.title("AI Resume Critiquer")
 st.markdown("Upload your resume and get AI powered feedback tailored to your needs")
